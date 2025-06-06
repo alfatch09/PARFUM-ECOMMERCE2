374e989ec284b7b11e63927edb4aaf494b6428e4
alfatch09 <alfatch09@gmail.com>
2025-06-06 15:20:22 +0700

    new2

--- frontend-app/src/store/modules/cart.js
+++
@@ -1,8 +0,0 @@
-mutations: {
-  ADD_TO_CART(state, product) {
-    state.items.push(product)
-  },
-  REMOVE_FROM_CART(state, productToRemove) {
-    state.items = state.items.filter(item => item._id !== productToRemove._id)
-  }
-}
